eea900d9a15ea4bd26509c5b3f74ca584732a6f2
shadi.altadouka <shadi.altadouka@yahoo.com>
2024-11-12 20:07:24 +0000

    frontend dockerfile change

--- docker/frontend/javascript/app.js
+++ docker/frontend/javascript/app.js
@@ -1,5 +1,5 @@
 async function loadTasks() {
-    const response = await fetch('http://localhost:5000/tasks');
+    const response = await fetch('http://4.154.227.82/tasks');
     const tasks = await response.json();
     const taskList = document.getElementById('task-list');
     taskList.innerHTML = tasks.map(task => `<li>${task.title}: ${task.description}</li>`).join('');
